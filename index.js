fc71541a53b87af7eaa3d1b9ff947a807c9bfcf6
rhino <ryan@perfectsensedigital.com>
2018-01-20 13:57:40 -0500

    Updates loader to export value

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 
-const process = require("./processor/object");
+const processor = require("./processor/object");
 
 module.exports = function(source) {
   if (this.cacheable) this.cacheable();
@@ -9,11 +9,11 @@ module.exports = function(source) {
   const loader = this;
   let value = typeof source === "string" ? JSON.parse(source) : source;
   const filePath = loader.resourcePath;
-  process(filePath, value);
+  processor(filePath, value);
 
   value = JSON.stringify(value)
     .replace(/\u2028/g, "\\u2028")
     .replace(/\u2029/g, "\\u2029");
 
-  return value;
+  return `module.exports = ${value}`;
 };
