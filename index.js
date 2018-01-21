847a875bd9d4d5da66fc7d37e5b89dbdb4ca24fd
rhino <ryan@perfectsensedigital.com>
2018-01-21 17:09:06 -0500

    Adds temporary workaround for storybook and create react app

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const fs = require("fs");
 const processor = require("./processor/object");
 
 module.exports = function(source) {
-  if (this.cacheable) this.cacheable();
-
   const loader = this;
+  if (loader.cacheable) loader.cacheable();
+
+  // Temporary hack to get this to work with storybook without changing extension
+  if (source.startsWith("module.exports = ")) {
+    source = source.replace("module.exports = ", "");
+  }
   let value = typeof source === "string" ? JSON.parse(source) : source;
   const filePath = loader.resourcePath;
   value = processor(filePath, value);
